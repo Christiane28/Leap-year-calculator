62b2a98ce4746ecf7776bda1ff78df747e5a21e5
Christiane28 <65940044+Christiane28@users.noreply.github.com>
2021-02-04 17:46:41 +1100

    Add files via upload

---
+++ leap year calculator #2.js
@@ -0,0 +1,19 @@
+function isLeap(year) {
+
+  if (year % 4 != 0) {
+    return "Not a leap year";
+  }
+
+  if (year % 4 === 0 && year % 100 != 0) {
+    return "Leap year";
+  }
+
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 != 0) {
+    return "Not a leap year";
+  }
+
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+    return "Leap year";
+  }
+
+}
